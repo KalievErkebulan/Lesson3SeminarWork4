653754e5ca53dc2d13b0d6c53238f2bae2bee3bb
Erkebulan <kaliev.e.m@gmail.com>
2023-01-23 23:56:07 +0600

    PractiseLesson3Work2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+double Sxyz(double xFirst,double yFirst, double zFirst, double xSecond, double ySecond, double zSecond)
+{
+    double distance = Math.Sqrt(Math.Pow(xFirst - xSecond,2) + Math.Pow(yFirst - ySecond,2) + Math.Pow(zFirst - zSecond,2));
+    return distance;
+}
+Console.WriteLine("Введите координаты xFirst : ");
+double xFirst = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты xSecond : ");
+double xSecond = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты yFirst : ");
+double yFirst = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты ySecond : ");
+double ySecond = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты zFirst : ");
+double zFirst = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты zSecond : ");
+double zSecond = double.Parse(Console.ReadLine()!);
+Sxyz(xFirst, xSecond, yFirst, ySecond, zFirst, zSecond);
+double d = Sxyz(xFirst, xSecond, yFirst, ySecond, zFirst, zSecond);
+Console.WriteLine();
+Console.WriteLine($"{d:f2}");
